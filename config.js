1b420622485115b816de098e7cd9be5961eebfa7
Kevyn <kevyncesare11@gmail.com>
2026-08-26 22:51:56 -0300

    Atualiza site e painel

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-window.SUPABASE_URL = "https://xokyvzjeoivmggsmkjjs.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_S2xtAJF5bSlkpKB4sNPEXg_-XJ5P_TE";
+const SUPABASE_URL =
+  "https://xokyvzjeoivmggsmkjjs.supabase.co";
+
+const SUPABASE_ANON_KEY =
+  "sb_publishable_S2xtAJF5bSlkpKB4sNPEXg_-XJ5P_TE";
